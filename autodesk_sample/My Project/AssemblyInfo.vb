cedf7dd7b3bdd41f5eaef1520f626700453ab62a
Georg Grebenyuk <55656376+GeorgGrebenyuk@users.noreply.github.com>
2022-04-22 15:17:00 +0300

    Copy base autodesk template

---
+++ autodesk_sample/My Project/AssemblyInfo.vb
@@ -0,0 +1,21 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SubassemblyScriptsMgd")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyProduct("SubassemblyScriptsMgd")> 
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7f6520de-0a5c-4254-8068-a1e3b4720031")> 
+
+
+'Add this attribute to avoid scanning all public types.
+<Assembly: Autodesk.AutoCAD.Runtime.ExtensionApplication(Nothing)> 
